845a6dc5eedd9ec0633bb49b870552ad9ec23910
thedern <dxsmith@soverin.net>
2019-03-30 14:25:57 -0400

    updated name of index to lowercase

--- models/Index.js
+++ models/Index.js
@@ -1,7 +1,7 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-    Article: require("./Article"),
-    Comments: require("./Comments")
+  Article: require('./Article'),
+  // updates
+  Comments: require('./Comments')
 };
-  
